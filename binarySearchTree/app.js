4ace96d7a90cf87b79f63874075e8454c3ff449c
Utpalkant Sahu <s.utpal3000@gmail.com>
2024-09-08 20:58:48 +0530

    bst-part-1

--- binarySearchTree/app.js
+++ binarySearchTree/app.js
@@ -2,7 +2,7 @@
 console.log("BST - Binary Search Tree\n----------");
 
 class BSTNode {
-    constructor(key){
+    constructor(key) {
         this.key = key;
         this.left = null
         this.right = null
@@ -10,36 +10,73 @@ class BSTNode {
 }
 
 class BinarySearchTree {
-    constructor(){
-        this.root=null
+    constructor() {
+        this.root = null
     }
 
-    insert(key){
+    insert(key) {
         const newNode = new BSTNode(key)
-        if (this.root=== null) {
+        if (this.root === null) {
             this.root = newNode
-        }else{
-            this.insertNode(this.root,newNode)
+        } else {
+            this.insertNode(this.root, newNode)
 
         }
     }
 
-    insertNode(node,newNode){ // node - as root node
-    if (newNode.key < node.key) {
-        if(node.left === null){
-            node.left=newNode
-        }else{
-            this.insertNode(node.left,newNode)
-        }
-    }else{
-        if (node.right === null) {
-            node.right = newNode
-        }else{
-            this.insertNode(node.right,newNode)
+    insertNode(node, newNode) { // node - as root node
+        if (newNode.key < node.key) {
+            if (node.left === null) {
+                node.left = newNode
+            } else {
+                this.insertNode(node.left, newNode)
+            }
+        } else {
+            if (node.right === null) {
+                node.right = newNode
+            } else {
+                this.insertNode(node.right, newNode)
+            }
         }
     }
+
+    delete(key) {
+        this.root = this.deleteNode(this.root, key)
+
     }
+
+    deleteNode(node, key) {
+        if (node == null) {
+            return null
+        }
+        if (key < node.key) {
+            node.left = this.deleteNode(node.left, key)
+        } else if (key > node.key) {
+            node.right = this.deleteNode(node.right, key)
+        } else {
+            if (node.left === null && node.right === null) {
+                return null
+            } else if (node.left === null) {
+                return node.right
+            } else if (node.right === null) {
+                return node.left
+            } else {
+                let tempNode = this.findMinNode(node.right) // right's smallest value
+                node.key = tempNode.key
+                node.right = this.deleteNode(node.right, tempNode.key)
+            }
+        }
+        return node
+    }
+
+    findMinNode(node){
+        while(node.left !== null){
+            node=node.left
+        }
+    }
+
 }
+
 
 // Testing
 let newNode = new BSTNode(10)
